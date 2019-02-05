32da7c251be19a41423883375622dcb4f0b3eb66
yvetteveve <yvetteumubyeyi2@gmail.com>
2019-02-05 14:19:09 +0200

    i will add background color in style.css

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -1,18 +1,22 @@
+import {GithubService} from '../github/github.service';
 import { Component, OnInit } from '@angular/core';
+import {Github} from '../github'
 
 @Component({
   selector: 'app-github',
   templateUrl: './github.component.html',
-  // providers:[githubService], //add the providers to the component
+  providers:[GithubService], //add the providers to the component
   styleUrls: ['./github.component.css']
 })
 export class GithubComponent implements OnInit {
 
-  // github:Github[];
-  constructor() {
+  github:Github;
+  constructor(githubService:GithubService) {
+    this.github = githubService.getGithub()
+  }
     // githubService:GithubService
     // this.github = githubService.getGithub()
-   }
+   
 
   ngOnInit() {
   }

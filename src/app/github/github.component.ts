423fe9e8f86b70bed2093faf7444435cb003e4ce
yvetteveve <yvetteumubyeyi2@gmail.com>
2019-02-05 17:05:02 +0200

    i editing a  link for github component.ts

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -1,6 +1,7 @@
 import {GithubService} from '../github/github.service';
 import { Component, OnInit } from '@angular/core';
-import {Github} from '../github'
+import {Github} from '../github-class/github'
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-github',
@@ -11,17 +12,15 @@ import {Github} from '../github'
 export class GithubComponent implements OnInit {
 
   github:Github;
-  constructor(githubService:GithubService) {
-    this.github = githubService.getGithub()
+  constructor(githubService:GithubService,private http:HttpClient) {
+    // this.github = githubService.getGithub();
   }
     // githubService:GithubService
     // this.github = githubService.getGithub()
    
 
   ngOnInit() {
-    ("'https://api.github.com/users/daneden?access_token=' + apiKey").subscribe(data=>{
-      // Successful API request.
+    this.http.get("'https://api.github.com/users/daneden?access_token=' + apiKey").subscribe(data=>{
   })
   }
-
 }
